2505c556729ff84771e77061f8160e8bccdf016b
Daniel Nikita Picugin <137338282+Regarnys@users.noreply.github.com>
2025-02-08 11:10:20 +0100

    Update atlas_i2c.py

--- atlas_i2c.py
+++ atlas_i2c.py
@@ -1,49 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+"""
+AtlasI2C driver for Atlas Scientific sensors.
+This module provides an interface for communicating with sensors over I2C.
+"""
 
 import io
 import sys
 import fcntl
 import time
 import copy
-import string
-
 
 class AtlasI2C:
-
-    # the timeout needed to query readings and calibrations
     LONG_TIMEOUT = 1.5
-    # timeout for regular commands
-    SHORT_TIMEOUT = .3
-    # the default bus for I2C on the newer Raspberry Pis, 
-    # certain older boards use bus 0
+    SHORT_TIMEOUT = 0.3
     DEFAULT_BUS = 1
-    # the default address for the sensor
     DEFAULT_ADDRESS = 98
     LONG_TIMEOUT_COMMANDS = ("R", "CAL")
-    SLEEP_COMMANDS = ("SLEEP", )
+    SLEEP_COMMANDS = ("SLEEP",)
 
-    def __init__(self, address=None, moduletype = "", name = "", bus=None):
-        '''
-        open two file streams, one for reading and one for writing
-        the specific I2C channel is selected with bus
-        it is usually 1, except for older revisions where its 0
-        wb and rb indicate binary read and write
-        '''
+    def __init__(self, address=None, moduletype="", name="", bus=None):
         self._address = address or self.DEFAULT_ADDRESS
         self.bus = bus or self.DEFAULT_BUS
         self._long_timeout = self.LONG_TIMEOUT
         self._short_timeout = self.SHORT_TIMEOUT
-        self.file_read = io.open(file="/dev/i2c-{}".format(self.bus), 
-                                 mode="rb", 
-                                 buffering=0)
-        self.file_write = io.open(file="/dev/i2c-{}".format(self.bus),
-                                  mode="wb", 
-                                  buffering=0)
+        self.file_read = io.open("/dev/i2c-{}".format(self.bus), mode="rb", buffering=0)
+        self.file_write = io.open("/dev/i2c-{}".format(self.bus), mode="wb", buffering=0)
         self.set_i2c_address(self._address)
         self._name = name
         self._module = moduletype
 
-	
     @property
     def long_timeout(self):
         return self._long_timeout
@@ -55,46 +40,31 @@ class AtlasI2C:
     @property
     def name(self):
         return self._name
-        
+
     @property
     def address(self):
         return self._address
-        
+
     @property
     def moduletype(self):
         return self._module
-        
-        
+
     def set_i2c_address(self, addr):
-        '''
-        set the I2C communications to the slave specified by the address
-        the commands for I2C dev using the ioctl functions are specified in
-        the i2c-dev.h file from i2c-tools
-        '''
         I2C_SLAVE = 0x703
         fcntl.ioctl(self.file_read, I2C_SLAVE, addr)
         fcntl.ioctl(self.file_write, I2C_SLAVE, addr)
         self._address = addr
 
     def write(self, cmd):
-        '''
-        appends the null character and sends the string over I2C
-        '''
         cmd += "\00"
         self.file_write.write(cmd.encode('latin-1'))
 
     def handle_raspi_glitch(self, response):
-        '''
-        Change MSB to 0 for all received characters except the first 
-        and get a list of characters
-        NOTE: having to change the MSB to 0 is a glitch in the raspberry pi, 
-        and you shouldn't have to do this!
-        '''
         if self.app_using_python_two():
             return list(map(lambda x: chr(ord(x) & ~0x80), list(response)))
         else:
             return list(map(lambda x: chr(x & ~0x80), list(response)))
-            
+
     def app_using_python_two(self):
         return sys.version_info[0] < 3
 
@@ -103,47 +73,35 @@ class AtlasI2C:
             response = [i for i in raw_data if i != '\x00']
         else:
             response = raw_data
-
         return response
 
     def response_valid(self, response):
         valid = True
         error_code = None
-        if(len(response) > 0):
-            
+        if len(response) > 0:
             if self.app_using_python_two():
                 error_code = str(ord(response[0]))
             else:
                 error_code = str(response[0])
-                
-            if error_code != '1': #1:
+            if error_code != '1':
                 valid = False
-
         return valid, error_code
 
     def get_device_info(self):
-        if(self._name == ""):
-            return self._module + " " + str(self.address)
+        if self._name == "":
+            return "{} {}".format(self._module, self.address)
         else:
-            return self._module + " " + str(self.address) + " " + self._name
-        
+            return "{} {} {}".format(self._module, self.address, self._name)
+
     def read(self, num_of_bytes=31):
-        '''
-        reads a specified number of bytes from I2C, then parses and displays the result
-        '''
-        
         raw_data = self.file_read.read(num_of_bytes)
         response = self.get_response(raw_data=raw_data)
-        #print(response)
         is_valid, error_code = self.response_valid(response=response)
-
         if is_valid:
             char_list = self.handle_raspi_glitch(response[1:])
-            result = "Success " + self.get_device_info() + ": " +  str(''.join(char_list))
-            #result = "Success: " +  str(''.join(char_list))
+            result = "Success " + self.get_device_info() + ": " + ''.join(char_list)
         else:
             result = "Error " + self.get_device_info() + ": " + error_code
-
         return result
 
     def get_command_timeout(self, command):
@@ -152,14 +110,9 @@ class AtlasI2C:
             timeout = self._long_timeout
         elif not command.upper().startswith(self.SLEEP_COMMANDS):
             timeout = self.short_timeout
-
         return timeout
 
     def query(self, command):
-        '''
-        write a command to the board, wait the correct timeout, 
-        and read the response
-        '''
         self.write(command)
         current_timeout = self.get_command_timeout(command=command)
         if not current_timeout:
@@ -173,9 +126,6 @@ class AtlasI2C:
         self.file_write.close()
 
     def list_i2c_devices(self):
-        '''
-        save the current address so we can restore it after
-        '''
         prev_addr = copy.deepcopy(self._address)
         i2c_devices = []
         for i in range(0, 128):
@@ -185,7 +135,6 @@ class AtlasI2C:
                 i2c_devices.append(i)
             except IOError:
                 pass
-        # restore the address we were using
         self.set_i2c_address(prev_addr)
-
         return i2c_devices
+
